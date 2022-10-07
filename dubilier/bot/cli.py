74dfc583b1c96a8e1cfa4fdbf565561f7197f216
Josh <josh@outsideorbit.com>
2022-10-07 11:45:33 -0500

    DUB-3: Corrected extra packages in setuptools config so they're actually installed.  Getting testing tooling in place.

--- dubilier/bot/cli.py
+++ dubilier/bot/cli.py
@@ -1,9 +1,19 @@
 """The primary CLI group for the bot entry point"""
 
 
+from email.policy import default
 import click
 
 
 @click.group()
+@click.pass_context
 def cli() -> None:
     """Main CLI group"""
+
+
+@cli.command()
+@cli.option("-db", default=None, help="Path to the sqlite database")
+@cli.option("-t", default=None, help="Token to auth to Discord")
+@click.pass_context
+def run(db: str, token: str) -> None:
+    """Start the bot daemon"""
