3044bd9f030f569aa06e3a435ff7304e7a10dd1f
Josh <josh@outsideorbit.com>
2022-10-07 11:53:13 -0500

    Fixing the click command options for the run command.

--- dubilier/bot/cli.py
+++ dubilier/bot/cli.py
@@ -1,7 +1,6 @@
 """The primary CLI group for the bot entry point"""
 
 
-from email.policy import default
 import click
 
 
@@ -12,8 +11,8 @@ def cli() -> None:
 
 
 @cli.command()
-@cli.option("-db", default=None, help="Path to the sqlite database")
-@cli.option("-t", default=None, help="Token to auth to Discord")
+@click.option("-db", default=None, help="Path to the sqlite database")
+@click.option("-t", default=None, help="Token to auth to Discord")
 @click.pass_context
 def run(db: str, token: str) -> None:
     """Start the bot daemon"""
