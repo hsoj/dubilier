a1a9884022dee0c83af48e50e0db1935a1d9e632
Josh <josh@outsideorbit.com>
2022-10-07 14:24:08 -0500

    Added CLI command for running the bot along with it's tests.
    gh-#3

--- dubilier/tests/test_bot_cli.py
+++ dubilier/tests/test_bot_cli.py
@@ -8,8 +8,12 @@ import dubilier.bot.cli
 class TestBotCLI(unittest.TestCase):
 
     def test_run(self) -> None:
+        args: list[str] = [
+            "run",
+            "-p test_db_path",
+            "-t test_token",
+        ]
         runner: click.testing.CliRunner = click.testing.CliRunner()
-        result: click.testing.Result = runner.invoke(dubilier.bot.cli.run,
-                                                     None,
-                                                     None)
-        self.assertEqual(result.exit_code, 1)
+        result: click.testing.Result = runner.invoke(dubilier.bot.cli.main,
+                                                     args)
+        self.assertEqual(result.exit_code, 0)
