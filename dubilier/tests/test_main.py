30d85f6604f4b2f251cec802eb394bf4353fbd2c
Josh <josh@outsideorbit.com>
2022-10-07 13:53:25 -0500

    Removing the old style of code so that it does not impact the testing. Adding testing for the current methods that exist.  Updated how tests are executed as well as expanded the tooling used within the pre commit.
    gh-#3

---
+++ dubilier/tests/test_main.py
@@ -0,0 +1,14 @@
+# pylint: disable=missing-docstring
+
+
+import unittest
+import unittest.mock
+import dubilier.__main__
+
+
+class TestMain(unittest.TestCase):
+
+    @unittest.mock.patch("dubilier.bot.cli.main")
+    def test_main_cli(self, mock: unittest.mock.Mock) -> None:
+        dubilier.__main__.main()
+        mock.assert_called()
