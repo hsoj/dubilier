98a3f6501ff8581001e4c13bac504bbd59e53f89
Josh <josh@outsideorbit.com>
2022-10-07 22:07:29 -0500

    Added version pinning in the setuptools configuration.
    Closes #8

--- setup.py
+++ setup.py
@@ -9,14 +9,19 @@ setuptools.setup(
     name="dubilier",
     version="0.0.1",
     install_requires=[
-        "click",
-        "discord.py",
-        "sqlalchemy",
+        "click==8.1.3",
+        "discord.py==2.0.1",
+        "sqlalchemy==1.4.41",
     ],
     extras_require={
         "dev": [
-            "pytest", "pylint", "tox", "mypy", "yapf", "pre-commit",
-            "coverage",
+            "pytest==7.1.3",
+            "pylint==2.15.3",
+            "tox==3.26.0",
+            "mypy==0.982",
+            "yapf==0.32.0",
+            "pre-commit==2.20.0",
+            "coverage==6.5.0",
         ]
     },
     entry_points={
