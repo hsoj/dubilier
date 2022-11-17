eb52dba19b2939b783fc6e59f1546ef5c08e27f3
Josh <josh@outsideorbit.com>
2022-11-16 18:18:23 -0600

    Added functionality to discover extensions.
    - Using `importlib.resources.contents` instead of `Loader.contents`
    - Added method to iterate over the contents and return just the module
      names
    
    GH: #18

--- dubilier/bot/discord.py
+++ dubilier/bot/discord.py
@@ -2,10 +2,15 @@
 # pylint: disable=too-few-public-methods
 
 
+import importlib.abc
+import importlib.machinery
+import importlib.resources
+import importlib.util
 import typing
+
 import discord
-import discord.flags
 import discord.ext.commands
+import discord.flags
 
 
 class Daemon(discord.ext.commands.Bot):
@@ -23,6 +28,39 @@ class Daemon(discord.ext.commands.Bot):
     def run(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         """Initializes the daemon process."""
         super().run(self.token, *args, **kwargs)
+
+    def find_extensions(self, package: str) -> list[str]:
+        """Finds extension modules within the provided package.
+
+        Arguments
+            package(str): Package to find extensions in
+
+        Returns:
+            list[str]: List of extensions found in the provided package
+        """
+        extensions: list[str] = []
+        contents: typing.Iterator[str] = importlib.resources.contents(package)
+        modules: list[str] = self.extract_extension_modules(
+            contents=list(contents),
+        )
+        extensions = [f"{package}.{x}" for x in modules]
+        return extensions
+
+    def extract_extension_modules(self, contents: list[str]) -> list[str]:
+        """Cleans up the provided contents input to determine the names
+        of the extension modules.
+
+        Arguments:
+            contents(list[str]): The contents found within the package.
+
+        Returns:
+            list[str]: List of actual extension modules.
+        """
+        modules: list[str] = []
+        for content in contents:
+            if "__" not in content:
+                modules.append(content.replace(".py", ""))
+        return modules
 
     async def setup_hook(self) -> None:
         """Hook method that is executed when the bot is ran which sets
--- dubilier/tests/test_bot_discord.py
+++ dubilier/tests/test_bot_discord.py
@@ -20,3 +20,32 @@ class TestDaemon(unittest.TestCase):
         daemon: dubilier.bot.discord.Daemon
         daemon = dubilier.bot.discord.Daemon(token="token", prefix="!")
         self.assertEqual(expected_prefix, daemon.command_prefix)
+
+    @unittest.mock.patch("importlib.resources.contents")
+    def test_find_extensions(self, content_mock: unittest.mock.MagicMock) -> None:
+        package: str = "test.package"
+        contents: list[str] = [
+            "__init__.py",
+            "file_one.py",
+            "file_two.py",
+        ]
+        expected: list[str] = [
+            f"{package}.file_one",
+            f"{package}.file_two"
+        ]
+        daemon: dubilier.bot.discord.Daemon
+        daemon = dubilier.bot.discord.Daemon(token="token")
+        content_mock.return_value = contents
+        self.assertEqual(expected, daemon.find_extensions(package))
+
+    def test_extract_extension_modules(self) -> None:
+        contents: list[str] = [
+            "__init__.py",
+            "file_one.py",
+            "file_two.py",
+        ]
+        expected: list[str] = ["file_one", "file_two"]
+        daemon: dubilier.bot.discord.Daemon
+        daemon = dubilier.bot.discord.Daemon(token="token")
+        actual: list[str] = daemon.extract_extension_modules(contents=contents)
+        self.assertEqual(expected, actual)
